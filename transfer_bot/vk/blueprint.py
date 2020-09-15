b739960301cd6d4d2101077448e295c88b6b5ba7
dhvcc <1337kwiz@gmail.com>
2020-09-16 00:25:21 +0300

    v1.2
    Added basic doc support
    (only types that are not instantly donloaded are allowed)
    Added HTML parsing support
    Updated code structure a bit
    It's still one file (but i don't care)
    
    Make sure you pass the vibe check...

--- transfer_bot/vk/blueprint.py
+++ transfer_bot/vk/blueprint.py
@@ -1,11 +1,14 @@
 import logging
 from asyncio import sleep
+from typing import Dict
+from typing import List
 
 import aiohttp
 import ujson
 from vkbottle.api import API
 from vkbottle.bot import Blueprint
 from vkbottle.bot import Message
+from vkbottle.types.objects.docs import Doc
 
 from transfer_bot.config.tg import BOT_TOKEN
 from transfer_bot.config.tg import DESTINATION_CONVERSATION_ID as DCI
@@ -16,88 +19,138 @@ logger = logging.getLogger()
 bot_bp = Blueprint()
 
 api_url = f"https://api.telegram.org/bot{BOT_TOKEN}/"
-tg_photo_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-message_text_template = "{sender}:\n" \
-                        "{text}"
+message_text_template = "<em>{sender}</em>:\n" \
+                        "<strong>{text}</strong>"
 
 
-def get_valid_attachments(message: Message):
-    return [attach for attach in message.attachments if attach.type in ("photo",)]
+class BadVibeException(Exception):
+    """Doctype not allowed"""
+
+    def __str__(self):
+        return "Поддерживаются только документы .pdf или .gif"
 
 
-async def get_sender(token: str, message: Message):
-    api = API(token)
-    user_list = await api.users.get(user_ids=str(message.from_id))
-    await sleep(0.1)
-    return user_list[0]
+class Utils:
+    @staticmethod
+    def get_valid_attachments(message: Message):
+        return [attach for attach in message.attachments if attach.type in ("photo", "doc")]
+
+    @staticmethod
+    async def get_sender(token: str, message: Message):
+        api = API(token)
+        user_list = await api.users.get(user_ids=str(message.from_id))
+        await sleep(0.1)
+        return user_list[0]
+
+    @staticmethod
+    def format_message(sender, message: Message):
+        return message_text_template.format(sender=f"{sender.last_name} {sender.first_name}",
+                                            text=message.text)[:1000]
+
+    @staticmethod
+    def basic_params():
+        return {
+            "random_id": bot_bp.extension.random_id(),
+            "chat_id": DCI,
+            "parse_mode": "HTML"
+        }
+
+    @staticmethod
+    async def vibe_check(response):
+        if response.status < 300:
+            logging.info(response.status)
+            response.close()
+            return True
+        else:
+            logging.warning(response.status)
+            logging.warning(await response.text())
+            response.close()
+            return False
 
 
-def format_message(sender, message: Message):
-    return message_text_template.format(sender=f"{sender.last_name} {sender.first_name}",
-                                        text=message.text)[:1000]
+class AttachmentHandlers:
+    @staticmethod
+    async def no_attachments(session, request_params, tg_message):
+        request_params["text"] = tg_message
+        response = await session.get(api_url + "sendMessage", params=request_params)
 
+        await Utils.vibe_check(response)
 
-def basic_params():
-    return {
-        "random_id": bot_bp.extension.random_id(),
-        "chat_id": DCI
-    }
+    @staticmethod
+    async def one_attachment(session, request_params, attachments, tg_message):
+        if attachments[0].type == "photo":
+            photo = attachments[0].photo
+            source = max(photo.sizes, key=lambda size: size.width)
+            request_params["photo"] = source.url
+            request_params["caption"] = tg_message
+            response = await session.get(api_url + "sendPhoto", params=request_params)
+            await Utils.vibe_check(response)
+        else:
+            doc = attachments[0].doc
+            request_params["document"] = doc.url
+            request_params["caption"] = tg_message
+            response = await session.get(api_url + "sendDocument", params=request_params)
+            if await Utils.vibe_check(response) is False:
+                raise BadVibeException
 
+    @staticmethod
+    async def more_attachments(session, request_params, attachments, tg_message):
+        media: List[Dict[str, str]] = []
+        documents: List[Doc] = []
+        for attach in attachments:
+            if attach.type == "photo":
+                photo = attach.photo
+                source = max(photo.sizes, key=lambda size: size.width)
+                media.append({
+                    "type": attach.type,
+                    "media": source.url,
+                })
+            else:
+                documents.append(attach.doc)
 
-async def no_attachments(session, request_params, tg_message):
-    request_params["text"] = tg_message
-    response = await session.get(api_url + "sendMessage", params=request_params)
-
-    return response
-
-
-async def one_attachment(session, request_params, attachments, tg_message):
-    photo = attachments[0].photo
-    source = max(photo.sizes, key=lambda size: size.width)
-    request_params["photo"] = source.url
-    request_params["caption"] = tg_message
-    response = await session.get(api_url + "sendPhoto", params=request_params)
-
-    return response
-
-
-async def more_attachments(session, request_params, attachments, tg_message):
-    request_params["media"] = []
-    for attach in attachments:
-        photo = attach.photo
-        source = max(photo.sizes, key=lambda size: size.width)
-        request_params["media"].append({
-            "type": attach.type,
-            "media": source.url,
-        })
-        request_params["media"][0]["caption"] = tg_message
-    request_params["media"] = ujson.dumps(request_params["media"])
-    response = await session.get(api_url + "sendMediaGroup", params=request_params)
-
-    return response
+        if media:
+            media[0]["caption"] = tg_message
+            request_params["media"] = ujson.dumps(media)
+            response = await session.get(api_url + "sendMediaGroup", params=request_params)
+            await sleep(0.1)
+            await Utils.vibe_check(response)
+        else:
+            bad_vibe = False
+            if not media:
+                request_params["caption"] = tg_message
+                request_params["document"] = documents[0].url
+                response = await session.get(api_url + "sendDocument", params=request_params)
+                if await Utils.vibe_check(response) is False:
+                    bad_vibe = True
+                documents = documents[1:]
+                request_params.pop("caption")
+            for doc in documents:
+                request_params["document"] = doc.url
+                await sleep(0.3)
+                response = await session.get(api_url + "sendDocument", params=request_params)
+                if await Utils.vibe_check(response) is False:
+                    bad_vibe = True
+            if bad_vibe:
+                raise BadVibeException
 
 
 @bot_bp.on.chat_message()
 async def handler(message: Message) -> None:
     """Default handler that catches any message"""
 
-    sender = await get_sender(GROUP_TOKEN, message)
-    request_params = basic_params()
-    tg_message = format_message(sender, message)
-    attachments = get_valid_attachments(message)
+    sender = await Utils.get_sender(GROUP_TOKEN, message)
+    request_params = Utils.basic_params()
+    tg_message = Utils.format_message(sender, message)
+    attachments = Utils.get_valid_attachments(message)
 
     async with aiohttp.ClientSession() as session:
-        if not attachments:
-            response = await no_attachments(session, request_params, tg_message)
-        elif len(attachments) == 1:
-            response = await one_attachment(session, request_params, attachments, tg_message)
-        else:
-            response = await more_attachments(session, request_params, attachments, tg_message)
-        # Response vibe check
-        if response.status < 300:
-            logging.info(response.status)
-        else:
-            logging.warning(response.status)
-            logging.warning(await response.text())
-        response.close()
-        await sleep(0.1)
+        try:
+            if not attachments:
+                await AttachmentHandlers.no_attachments(session, request_params, tg_message)
+            elif len(attachments) == 1:
+                await AttachmentHandlers.one_attachment(session, request_params, attachments, tg_message)
+            else:
+                await AttachmentHandlers.more_attachments(session, request_params, attachments, tg_message)
+        except BadVibeException as e:
+            await message(str(e))
+    await sleep(0.1)
